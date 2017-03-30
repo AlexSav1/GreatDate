8ffefd1fda432d14e2ee8df3f364dd7c1dc4af68
Avery <imthatninjatho@gmail.com>
2017-03-30 19:08:44 -0400

    added loactionVC

---
+++ GreatDate/LocationViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocationViewController.h
+//  GreatDate
+//
+//  Created by Avery Barrantes on 3/30/17.
+//  Copyright © 2017 Alex Laptop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationViewController : UIViewController
+- (IBAction)currentLocationButton:(UIButton *)sender;
+- (IBAction)zipCodeButton:(UIButton *)sender;
+
+@end
