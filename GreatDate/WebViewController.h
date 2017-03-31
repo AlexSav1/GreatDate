495ee33f75b0bf83df34bae59cdb2d5989fd683f
Max Schneider <mschn0405@gmail.com>
2017-03-31 13:11:05 -0400

    added WKWebviewcontroller

---
+++ GreatDate/WebViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebViewController.h
+//  GreatDate
+//
+//  Created by Maxwell Schneider on 3/31/17.
+//  Copyright © 2017 Alex Laptop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DateLocation.h"
+
+@interface WebViewController : UIViewController
+@property NSURL *url;
+@property DateLocation *selectedLocation;
+@end
---
+++ GreatDate/WebViewController.m
@@ -0,0 +1,52 @@
+//
+//  WebViewController.m
+//  GreatDate
+//
+//  Created by Maxwell Schneider on 3/31/17.
+//  Copyright © 2017 Alex Laptop. All rights reserved.
+//
+
+#import "WebViewController.h"
+#import <WebKit/WebKit.h>
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//  **** Setup config, webview, and URLrequest ****
+    WKWebViewConfiguration *webConfig = [[WKWebViewConfiguration alloc]init];
+    WKWebView *webView = [[WKWebView alloc]initWithFrame:self.view.frame configuration:webConfig];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]init];
+    
+    request.URL = self.url;
+    
+    NSString *convertedString = [self.selectedLocation.name stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+    
+    NSLog(@"%@", convertedString);
+    
+    
+    if([request.URL isEqual:nil]){ request.URL = [NSURL URLWithString:[NSString stringWithFormat:@"https://www.google.com/#q=%@&*", convertedString]];
+    }
+    
+    if(webView.isLoading){
+        
+    }
+    
+    NSURLRequest *productRequest = [NSURLRequest requestWithURL:request.URL];
+    
+    [webView loadRequest:productRequest];
+    
+    [self.view addSubview:webView];
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
